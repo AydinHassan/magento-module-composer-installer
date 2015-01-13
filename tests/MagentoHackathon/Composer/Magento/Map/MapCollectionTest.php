72ec1b44c3aca7aabdd087658339444f01cf1e4d
Aydin Hassan <aydin@hotmail.co.uk>
2015-01-13 22:15:48 +0000

    cover new functions

--- tests/MagentoHackathon/Composer/Magento/Map/MapCollectionTest.php
+++ tests/MagentoHackathon/Composer/Magento/Map/MapCollectionTest.php
@@ -2,6 +2,7 @@
 
 namespace MagentoHackathon\Composer\Magento\Map;
 
+use JsonSchema\Constraints\Collection;
 use PHPUnit_Framework_TestCase;
 
 /**
@@ -55,11 +56,12 @@ class MapCollectionTest extends PHPUnit_Framework_TestCase
         $collection = new MapCollection($items);
         $this->assertCount(4, $collection);
 
-        $collection->remove($map2);
-        $this->assertCount(3, $collection);
+        $newCollection = $collection->remove($map2);
+        $this->assertCount(4, $collection);
+        $this->assertCount(3, $newCollection);
 
         $updatedItems = array($map1, $map3, $map4);
-        $this->assertSame($updatedItems, iterator_to_array($collection));
+        $this->assertSame($updatedItems, iterator_to_array($newCollection));
     }
 
     public function testReplaceMap()
@@ -76,12 +78,13 @@ class MapCollectionTest extends PHPUnit_Framework_TestCase
         $newMap1 = new Map('new-source', 'new-destination', '/tmp/', '/tmp/');
         $newMap2 = new Map('new-source1', 'new-destination1', '/tmp/', '/tmp/');
 
-        $collection->replace($map2, array($newMap1, $newMap2));
+        $newCollection = $collection->replace($map2, array($newMap1, $newMap2));
 
         $expectedItems = array($map1, $newMap1, $newMap2, $map3);
 
-        $this->assertCount(4, $collection);
-        $this->assertSame($expectedItems, iterator_to_array($collection));
+        $this->assertCount(4, $newCollection);
+        $this->assertCount(3, $collection);
+        $this->assertSame($expectedItems, iterator_to_array($newCollection));
     }
 
     public function testReplaceThrowsExceptionIfReplacementArrayContainsElementWhichIsNotAMap()
@@ -111,5 +114,42 @@ class MapCollectionTest extends PHPUnit_Framework_TestCase
 
         $this->setExpectedException('InvalidArgumentException', 'Map does not belong to this collection');
         $collection->replace(new Map('nope', 'nope', 'nope', 'nope'), array());
+    }
+
+    public function testAllReturnsAllItems()
+    {
+        $map1 = new Map('source', 'destination', '/tmp/', '/tmp/');
+        $map2 = new Map('source1', 'destination1', '/tmp/', '/tmp/');
+        $map3 = new Map('source2', 'destination2', '/tmp/', '/tmp/');
+
+        $items = array($map1, $map2, $map3);
+
+        $collection = new MapCollection($items);
+        $this->assertSame($items, $collection->all());
+    }
+
+    public function testFilterThrowsExceptionIfArgumentNotCallable()
+    {
+        $collection = new MapCollection(array());
+        $this->setExpectedException('InvalidArgumentException', 'Expected callable, got "stdClass"');
+        $collection->filter(new \stdClass);
+    }
+
+    public function testFilter()
+    {
+        $map1 = new Map('source', 'destination', '/tmp/', '/tmp/');
+        $map2 = new Map('source1', 'destination1', '/tmp/', '/tmp/');
+        $map3 = new Map('source2', 'destination2', '/tmp/', '/tmp/');
+
+        $items = array($map1, $map2, $map3);
+
+        $collection = new MapCollection($items);
+
+        $filtered = $collection->filter(function (Map $map) {
+            return $map->getSource() === 'source';
+        });
+
+
+        $this->assertSame(array($map1), $filtered->all());
     }
 }
